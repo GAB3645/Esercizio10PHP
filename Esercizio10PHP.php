5b659950cc58a4a140184a9eae87ef520caac069
Gabriel <gabrielsiano2006@gmail.com>
2024-10-03 09:15:24 +0300

    first commit

---
+++ Esercizio10PHP.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    $size = 25;
+    $num = 7;
+
+
+
+    for ($i = 0; $i < $num; $i++) {
+
+        echo "<p style='font-size: {$size}px;'> QUESTO E' UN PARAGRAFO </p>";
+        $size = $size + 2;
+    }
+
+
+
+    ?>
+
+</body>
+</html>
